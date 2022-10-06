e08fdba74c2aa4cb4891b0761b4bc78f5ca967bb
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-10-06 19:32:33 +0200

    add structures

---
+++ structures/board.c
@@ -0,0 +1,9 @@
+#include "player.c"
+//
+// Created by Antoine TOUZART on 06/10/2022.
+//
+typedef struct {
+    int row;
+    int column;
+    Player **state;
+} Board;
---
+++ structures/player.c
@@ -0,0 +1,9 @@
+//
+// Created by Antoine TOUZART on 06/10/2022.
+//
+
+typedef struct {
+    char *color;
+    char token;
+    int playedTurnCount;
+} Player;
